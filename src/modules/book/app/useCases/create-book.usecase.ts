c6eb8063c2c32a1f9c0d2072f779e1534e855f54
lucasbasep <lucas.base29@gmail.com>
2025-06-30 22:32:33 -0300

    refactor: add a date property adapter

--- src/modules/book/app/useCases/create-book.usecase.ts
+++ src/modules/book/app/useCases/create-book.usecase.ts
@@ -12,7 +12,11 @@ export class CreateBookUseCase implements ICreateBookUseCase {
     private createBookS: CreateBookService,
   ) {}
   public async handle(bookData: CreateBookDTO): Promise<Book> {
-    const book = await this.createBookS.execute(bookData);
+    const time = new Date(bookData.publicationDate).toISOString();
+    const book = await this.createBookS.execute({
+      ...bookData,
+      publicationDate: time,
+    });
     return book;
   }
 }
