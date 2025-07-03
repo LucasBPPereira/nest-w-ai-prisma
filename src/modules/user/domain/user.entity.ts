4b8398d524020d51bc0a68bf116f442c7d7349c8
lucasbasep <lucas.base29@gmail.com>
2025-07-03 02:16:06 -0300

    refactor: alter User class to User interface

--- src/modules/user/domain/user.entity.ts
+++ src/modules/user/domain/user.entity.ts
@@ -1,9 +1,9 @@
 import { User as PrismaUser } from '@prisma/client';
-export class User implements PrismaUser {
-  public readonly id: string;
-  public readonly name: string;
-  public readonly email: string;
-  public readonly password: string;
-  public readonly createdAt: Date;
-  public readonly updatedAt: Date;
+export interface User extends PrismaUser {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
